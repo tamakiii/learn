f822bb19eff87fc640fc1a06502d92d553da73d8
tamakiii <tamaki.htm@gmail.com>
2017-06-20 12:18:21 +0900

    fm

--- template-engine/hogan/index.js
+++ template-engine/hogan/index.js
@@ -1,17 +1,20 @@
 // require hogan
 var hogan = require("hogan.js");
 
-// compile template
-var template = hogan.compile("@{{name}}");
+console.log(hogan.compile, hogan.template);
+// hogan.template = 'templates/layout.mustache';
 
-var team = ['dhg', 'fat', 'jimio', 'nickgreen', 'sayrer'];
-
-team.map(function (twitterer) {
-
-  // Render context to template
-  return template.render({name: twitterer });
-
-});
-
-// outputs "Follow: @dhg @fat @jimio @nickgreen @sayrer!"
-console.log('Follow: ' + team.join(' ') + '!');
+// // compile template
+// var template = hogan.compile("@{{name}}");
+//
+// var team = ['dhg', 'fat', 'jimio', 'nickgreen', 'sayrer'];
+//
+// team.map(function (twitterer) {
+//
+//   // Render context to template
+//   return template.render({name: twitterer });
+//
+// });
+//
+// // outputs "Follow: @dhg @fat @jimio @nickgreen @sayrer!"
+// console.log('Follow: ' + team.join(' ') + '!');
