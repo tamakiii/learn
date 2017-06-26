efdd46d2b0b1bc02e023c11fdeaed2bfd2046b29
tamakiii <tamaki.htm@gmail.com>
2017-06-26 22:59:44 +0900

    use http.Handle()

--- go/tutorial/golang-org/pkg/net/http/server.go
+++ go/tutorial/golang-org/pkg/net/http/server.go
@@ -5,11 +5,13 @@ import (
   "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello, World")
+type String string
+
+func (s String) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprint(w, s)
 }
 
 func main() {
-  http.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
-  http.ListenAndServe(":8080", nil)
+  http.Handle("/", String("Hello World."))
+  http.ListenAndServe("localhost:8000", nil)
 }
