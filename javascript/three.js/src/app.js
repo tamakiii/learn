cacdd0b727e51b52653032ca8e6395b7389f7219
tamakiii <tamaki.htm@gmail.com>
2017-08-02 01:05:19 +0900

    impl people list

--- javascript/three.js/src/app.js
+++ javascript/three.js/src/app.js
@@ -1,2 +1,22 @@
+import {groupBy} from 'lodash/collection'
+
+const people = [{
+  manager: 'Jen',
+  name: 'Bob'
+}, {
+  manager: 'Jen',
+  name: 'Sue'
+}, {
+  manager: 'Bob',
+  name: 'Shirley'
+}, {
+  manager: 'Bob',
+  name: 'Terrence'
+}]
+const managerGroups = groupBy(people, 'manager')
+
 const root = document.querySelector('#root')
-root.innerHTML = `<p>Hello webpack.</p>`
+root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
+
+// const root = document.querySelector('#root')
+// root.innerHTML = `<p>Hello webpack.</p>`
