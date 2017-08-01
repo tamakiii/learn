1fac54d9207d9b59ebbb4df7760dd97acd454461
tamakiii <tamaki.htm@gmail.com>
2017-08-02 02:08:39 +0900

    built

--- javascript/three.js/dist/admin.bundle.js
+++ javascript/three.js/dist/admin.bundle.js
@@ -1,16 +1,1 @@
-webpackJsonp([2],{
-
-/***/ 218:
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__people__ = __webpack_require__(74);
-
-
-var root = document.querySelector('#root');
-root.innerHTML = '<p>There are ' + __WEBPACK_IMPORTED_MODULE_0__people__["a" /* default */].length + ' people.</p>';
-
-/***/ })
-
-},[218]);
+webpackJsonp([2],{"./admin.js":function(e,n,o){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var r=o("./people.js");document.querySelector("#root").innerHTML="<p>There are "+r.a.length+" people.</p>"}},["./admin.js"]);
