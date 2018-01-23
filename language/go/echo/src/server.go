3a68271277bfba102e8d8998892a1039e2ee4dda
tamakiii <tamaki.htm@gmail.com>
2018-01-23 22:58:42 +0900

    add .Post and .Static

--- language/go/echo/src/server.go
+++ language/go/echo/src/server.go
@@ -7,9 +7,22 @@ import (
   "io"
 )
 
+type User struct {
+  Name string `json:"name" xml:"name" form:"name" query:"name"`
+  Email string `json:"email" xml:"email" form:"email" query:"email"`
+}
+
 func getUser(c echo.Context) error {
   id := c.Param("id")
   return c.String(http.StatusOK, id)
+}
+
+func postUser(c echo.Context) error {
+  u := new(User)
+  if err := c.Bind(u); err != nil {
+    return err
+  }
+  return c.JSON(http.StatusCreated, u)
 }
 
 func show(c echo.Context) error {
@@ -49,7 +62,9 @@ func main() {
     return c.String(http.StatusOK, "Hello, World!")
   })
   e.GET("/users/:id", getUser)
+  e.POST("/users", postUser)
   e.GET("/show", show)
   e.POST("/save", save)
+  e.Static("/static", "src/static")
   e.Logger.Fatal(e.Start(":1323"))
 }
