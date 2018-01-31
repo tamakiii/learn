3c3b72ea27d22956af682122788300b47e825553
tamakiii <tamaki.htm@gmail.com>
2018-02-01 00:33:45 +0900

    fix

--- graphic/opengl/tutorial/003-display-teapot/main.cpp
+++ graphic/opengl/tutorial/003-display-teapot/main.cpp
@@ -2,6 +2,7 @@
 
 void display()
 {
+  glClear(GL_COLOR_BUFFER_BIT);
   glutWireTeapot(1);
   glFlush();
 }
@@ -12,7 +13,7 @@ int main(int argc, char *argv[])
   glutInitWindowPosition(640, 0);
   glutInitWindowSize(640, 640);
   glutCreateWindow("Title");
-  glClearColor(1, 1, 1, 0);
+  glClearColor(0, 0, 0, 1.0f);
   glutDisplayFunc(display);
   glutMainLoop();
 }
