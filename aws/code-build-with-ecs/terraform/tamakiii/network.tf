7ecc840af5987f754e6b7bdd05e110a05932ae17
tamakiii <tamaki.htm@gmail.com>
2018-06-19 17:34:39 +0900

    add subnets

--- aws/code-build-with-ecs/terraform/tamakiii/network.tf
+++ aws/code-build-with-ecs/terraform/tamakiii/network.tf
@@ -10,3 +10,23 @@ resource "aws_vpc" "sample_app" {
     Name = "sample-app"
   }
 }
+
+resource "aws_subnet" "sample_app_public_a" {
+  vpc_id = "${aws_vpc.sample_app.id}"
+  cidr_block = "${var.CIDR_SAMPLE_APP_BLOCKS["public_a"]}"
+  availability_zone = "${var.AWS_DEFAULT_REGION}a"
+
+  tags {
+    Name = "sample-app/public-a"
+  }
+}
+
+resource "aws_subnet" "sample_app_public_c" {
+  vpc_id = "${aws_vpc.sample_app.id}"
+  cidr_block = "${var.CIDR_SAMPLE_APP_BLOCKS["public_c"]}"
+  availability_zone = "${var.AWS_DEFAULT_REGION}c"
+
+  tags {
+    Name = "sample-app/public-c"
+  }
+}
--- aws/code-build-with-ecs/terraform/tamakiii/output.tf
+++ aws/code-build-with-ecs/terraform/tamakiii/output.tf
@@ -6,5 +6,14 @@ VPC:
     id: ${aws_vpc.sample_app.id}
     cidr_block: ${aws_vpc.sample_app.cidr_block}
 
+Subnet:
+  ${aws_subnet.sample_app_public_a.tags.Name}:
+    id: ${aws_subnet.sample_app_public_a.id}:
+    cidr_block: ${aws_subnet.sample_app_public_a.cidr_block}
+
+  ${aws_subnet.sample_app_public_c.tags.Name}:
+    id: ${aws_subnet.sample_app_public_c.id}:
+    cidr_block: ${aws_subnet.sample_app_public_a.cidr_block}
+
 EOT
 }
